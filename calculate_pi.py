ed30380faf9bf5509d6dc15326e073fc87b0f5a9
kmart <kevinmartinmail@gmail.com>
2024-11-18 13:06:37 -0800

    Create calculate_pi.py

---
+++ calculate_pi.py
@@ -0,0 +1,10 @@
+import math
+
+def calculate_pi():
+    """
+    Simple function to return the value of pi using math library.
+    """
+    return math.pi
+
+if __name__ == "__main__":
+    print(calculate_pi())
